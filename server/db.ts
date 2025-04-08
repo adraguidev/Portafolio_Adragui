771fded66c7d4f83e7e30d9497e73595af2aeb51
Adrian Aguirre <66138639+adraguidev@users.noreply.github.com>
2025-04-08 04:36:03 +0000

    fix db.ts for Heroku

--- server/db.ts
+++ server/db.ts
@@ -1,10 +1,12 @@
 import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
+import pkg from "pg"; // 👈 CommonJS compat
 import * as schema from "@shared/schema";
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL must be set.");
 }
+
+const { Pool } = pkg; // 👈 así accedemos correctamente
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
